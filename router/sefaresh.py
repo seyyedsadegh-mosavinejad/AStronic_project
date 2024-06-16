b340d68acd16e6561e389800d763e8b4c97d4c09
ssadegh mosav <ssadegh.mosav@gmail.com>
2024-06-16 18:04:39 +0330

    sefaresh 1

--- router/sefaresh.py
+++ router/sefaresh.py
@@ -99,131 +99,131 @@ async def sabt_sefaresh(addressid: int, raveshersalid: int, current_user: Annota
         "message": f"سفارش شما با کد {sefaresh.sid} با موفقیت ثبت شد. ودر وضعیت منتظر پرداخت قرار دارد "
     }
 
-
-@router.get("/remove")
-async def remove_product(spid: int,response:Response,
-                      current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
-    user = get_user_by_phone(current_user.get('sub'))
-    uid = user.uid
-
-    product = session.query(SubProduct).filter(SubProduct.spid == spid).first()
-    if not product:
-        return {
-            "error": "چنین محصولی یافت نشد"
-        }
-    cart = session.query(Cart).filter(and_(Cart.uid == uid, Cart.spid == spid)).first()
-    if not cart:
-
-        return {
-            "message": "این محصول از قبل وجود نداشته است"
-        }
-
-    session.delete(cart)
-    session.commit()
-
-    return {
-        "message": "محصول مورد نظر از سبد خرید حذف شد"
-    }
-
-
-
-@router.get("/removeall")
-async def remove_all(response:Response,
-                      current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
-    user = get_user_by_phone(current_user.get('sub'))
-    uid = user.uid
-
-
-    carts = session.query(Cart).filter(Cart.uid == uid).all()
-    if not carts or len(carts) == 0:
-
-        return {
-            "message": "سبد خرید شما خالی است"
-        }
-    for cart in carts:
-        session.delete(cart)
-        session.commit()
-
-    return {
-        "message": "سبد خرید شما خالی شد"
-    }
-
-
-@router.get("/get")
-async def get_product(response:Response,
-                      current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
-    user = get_user_by_phone(current_user.get('sub'))
-    uid = user.uid
-
-    carts = session.query(Cart).filter(Cart.uid == uid).all()
-    if not carts or len(carts) == 0:
-
-        return {
-            "message": "سبد خرید شما خالی است"
-        }
-    mycart = []
-    for cart in carts:
-        d = {}
-        d["spid"] = cart.spid
-        d["tedad"] = cart.tedad
-        mycart.append(d)
-
-    return mycart
-
-@router.get("/getnumber")
-async def get_number(response:Response,
-                      current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
-
-    user = get_user_by_phone(current_user.get('sub'))
-    uid = user.uid
-
-    carts = session.query(Cart).filter(Cart.uid == uid).all()
-    if not carts or len(carts) == 0:
-
-        return 0
-    c = 0
-    for cart in carts:
-        c += cart.tedad
-
-    return c
-
-
-@router.get("/settedad")
-async def set_product(spid: int, tedad: int, response:Response,
-                      current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
-    user = get_user_by_phone(current_user.get('sub'))
-    uid = user.uid
-
-    product = session.query(SubProduct).filter(SubProduct.spid == spid).first()
-    if not product:
-        return {
-            "error": "چنین محصولی یافت نشد"
-        }
-
-    cart = session.query(Cart).filter(and_(Cart.uid == uid, Cart.spid == spid)).first()
-    if tedad > product.mojoodi:
-        return {
-            "message": "موجودی محصول کمتر از تعداد وارد شده است"
-        }
-    if not cart :
-        cart = Cart(
-            uid=uid,
-            spid=spid,
-            tedad=tedad
-        )
-        session.add(cart)
-        session.commit()
-        return {
-            "message": "محصول مورد نظر به سبد خرید اضافه شد"
-        }
-    cart.tedad = tedad
-    session.commit()
-
-    return {
-            "message": "محصول مورد نظر به سبد خرید اضافه شد"
-        }
-
-
+#
+# @router.get("/remove")
+# async def remove_product(spid: int,response:Response,
+#                       current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
+#     user = get_user_by_phone(current_user.get('sub'))
+#     uid = user.uid
+#
+#     product = session.query(SubProduct).filter(SubProduct.spid == spid).first()
+#     if not product:
+#         return {
+#             "error": "چنین محصولی یافت نشد"
+#         }
+#     cart = session.query(Cart).filter(and_(Cart.uid == uid, Cart.spid == spid)).first()
+#     if not cart:
+#
+#         return {
+#             "message": "این محصول از قبل وجود نداشته است"
+#         }
+#
+#     session.delete(cart)
+#     session.commit()
+#
+#     return {
+#         "message": "محصول مورد نظر از سبد خرید حذف شد"
+#     }
+#
+#
+#
+# @router.get("/removeall")
+# async def remove_all(response:Response,
+#                       current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
+#     user = get_user_by_phone(current_user.get('sub'))
+#     uid = user.uid
+#
+#
+#     carts = session.query(Cart).filter(Cart.uid == uid).all()
+#     if not carts or len(carts) == 0:
+#
+#         return {
+#             "message": "سبد خرید شما خالی است"
+#         }
+#     for cart in carts:
+#         session.delete(cart)
+#         session.commit()
+#
+#     return {
+#         "message": "سبد خرید شما خالی شد"
+#     }
+#
+#
+# @router.get("/get")
+# async def get_product(response:Response,
+#                       current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
+#     user = get_user_by_phone(current_user.get('sub'))
+#     uid = user.uid
+#
+#     carts = session.query(Cart).filter(Cart.uid == uid).all()
+#     if not carts or len(carts) == 0:
+#
+#         return {
+#             "message": "سبد خرید شما خالی است"
+#         }
+#     mycart = []
+#     for cart in carts:
+#         d = {}
+#         d["spid"] = cart.spid
+#         d["tedad"] = cart.tedad
+#         mycart.append(d)
+#
+#     return mycart
+#
+# @router.get("/getnumber")
+# async def get_number(response:Response,
+#                       current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
+#
+#     user = get_user_by_phone(current_user.get('sub'))
+#     uid = user.uid
+#
+#     carts = session.query(Cart).filter(Cart.uid == uid).all()
+#     if not carts or len(carts) == 0:
+#
+#         return 0
+#     c = 0
+#     for cart in carts:
+#         c += cart.tedad
+#
+#     return c
+#
+#
+# @router.get("/settedad")
+# async def set_product(spid: int, tedad: int, response:Response,
+#                       current_user: Annotated[UserAuth, Depends(get_current_active_user)]):
+#     user = get_user_by_phone(current_user.get('sub'))
+#     uid = user.uid
+#
+#     product = session.query(SubProduct).filter(SubProduct.spid == spid).first()
+#     if not product:
+#         return {
+#             "error": "چنین محصولی یافت نشد"
+#         }
+#
+#     cart = session.query(Cart).filter(and_(Cart.uid == uid, Cart.spid == spid)).first()
+#     if tedad > product.mojoodi:
+#         return {
+#             "message": "موجودی محصول کمتر از تعداد وارد شده است"
+#         }
+#     if not cart :
+#         cart = Cart(
+#             uid=uid,
+#             spid=spid,
+#             tedad=tedad
+#         )
+#         session.add(cart)
+#         session.commit()
+#         return {
+#             "message": "محصول مورد نظر به سبد خرید اضافه شد"
+#         }
+#     cart.tedad = tedad
+#     session.commit()
+#
+#     return {
+#             "message": "محصول مورد نظر به سبد خرید اضافه شد"
+#         }
+#
+#
 
 
 
